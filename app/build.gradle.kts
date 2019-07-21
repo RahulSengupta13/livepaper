8873c380aa43538dd50776d541e227f8c0760316
Rahul Sengupta <rahul.sengupta@bottlerocketstudios.com>
2019-07-21 02:22:22 -0500

    Add a couple dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,15 +32,13 @@ androidExtensions {
 }
 
 dependencies {
+    implementation(project(":network"))
+
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40")
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("androidx.core:core-ktx:1.2.0-alpha02")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    testImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.2")
     implementation("com.jakewharton.timber:timber:4.7.1")
 
@@ -48,20 +46,29 @@ dependencies {
     implementation("androidx.paging:paging-runtime-ktx:2.1.0")
 
     //lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime:2.0.0")
-    implementation("androidx.lifecycle:lifecycle-compiler:2.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0")
 
     //navigation
-    implementation("androidx.navigation:navigation-runtime:2.1.0-beta02")
-    implementation("androidx.navigation:navigation-runtime-ktx:2.1.0-beta02")
-    implementation("androidx.navigation:navigation-fragment:2.1.0-beta02")
     implementation("androidx.navigation:navigation-fragment-ktx:2.1.0-beta02")
-    implementation("androidx.navigation:navigation-ui:2.1.0-beta02")
     implementation("androidx.navigation:navigation-ui-ktx:2.1.0-beta02")
-    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.1.0-beta02")
 
     //material
     implementation("com.google.android.material:material:1.1.0-alpha08")
+
+    //di
+    implementation("org.koin:koin-android:2.0.1")
+    implementation("org.koin:koin-android-viewmodel:2.0.1")
+
+    //joda time
+    implementation("joda-time:joda-time:2.10.2")
+
+    //room
+    implementation("androidx.room:room-runtime:2.1.0")
+    implementation("androidx.room:room-ktx:2.1.0")
+    kapt("androidx.room:room-compiler:2.1.0")
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
